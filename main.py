a424b5e989cad3d65ad415eb189d95616cd9f7c1
Facundo Marcet <facundo.marcet@sdc.com.ar>
2021-01-09 23:47:28 -0300

    Recognizing specific people

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
 import cv2
 import time
+import pickle
 
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 smile_cascade = cv2.CascadeClassifier("haarcascade_smile.xml")
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("trainner.yml")
+
+labels = {"person_name": 1}
+with open("labels.pickle", "rb") as f:
+    og_labels = pickle.load(f)
+    labels = {v:k for k,v in og_labels.items()}
 
 video = cv2.VideoCapture(0)
 
 while True:
     check, frame = video.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    face = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5)
-    for x, y, w, h in face:
-        img = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 3)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5)
+    for (x, y, w, h) in faces:
+        roi_gray = gray[y:y+h, x:x+w]
+        id_, conf = recognizer.predict(roi_gray)
+        print(labels[id_], conf)
+        if conf >= 45 and conf <= 85:
+            #print(id_)
+            #print(labels[id_])
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            name = labels[id_]
+            color = (255, 255, 255)
+            stroke = 2
+            cv2.putText(frame, name, (x, y), font, 1, color, stroke, cv2.LINE_AA)
+
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 3)
         smile = smile_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=20)
-        for x, y, w, h in smile:
-            img = cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 3)
+        for (x, y, w, h) in smile:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 3)
     cv2.imshow('detected', frame)
     key = cv2.waitKey(1)
 
